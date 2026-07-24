85bc17ff789768dd5d418c46cae380995ffb9731
joohan8080 <joohan8080@gmail.com>
2026-07-24 15:08:08 +0900

    사주궁합프리미엄공유버튼 수정

--- resources/js/common.js
+++ resources/js/common.js
@@ -26,9 +26,7 @@ const headerContent = `
 `;
 const stickyBottom = `
 
-    <div class="shareBtn">
-        <div><img src="https://land.withusmk.co.kr/assets/saju/resources/img/ui/share.webp" alt=""></div>
-    </div>
+
 
     <div class="sticky_Bottom_BarBox">
             <img src="https://land.withusmk.co.kr/assets/saju/resources/img/ui/sticky_Bottom_Bar_frame.webp" alt="" style=" pointer-events: none;">
